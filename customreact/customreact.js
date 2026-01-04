8cc2076d87ef467ee14eafb211e67dabcc6a8b5c
Tanishq <your-tanishqsejwar9752@gmail.com>
2026-01-04 20:23:09 +0530

    first

---
+++ customreact/customreact.js
@@ -0,0 +1,9 @@
+// const containor = document.getElementById("#root")
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    Element
+);
+const Element = (
+    <a href="www.google.com" target="_blank">visit google </a>
+)
